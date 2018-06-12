eae7864956d2065b377b85511a73a6f55ea81835
vano <kolesen.ivan@gmail.com>
2018-06-12 09:14:25 +0300

    included prettier

--- js/dict.js
+++ js/dict.js
@@ -1,76 +1,171 @@
 export const dictMonster = {
-    "headsIdle" : ['spriteMonsterHeadIdle_first', 'spriteMonsterHeadIdle_second','spriteMonsterHeadIdle_third'],
-    "bodiesIdle" : ['spriteMonsterBodyIdle_first', 'spriteMonsterBodyIdle_second','spriteMonsterBodyIdle_third'],
-    "legsIdle" : ['spriteMonsterLegsIdle_first', 'spriteMonsterLegsIdle_second','spriteMonsterLegsIdle_third'],
-    "headsFire" : ['spriteMonsterHeadFire_first', 'spriteMonsterHeadFire_second','spriteMonsterHeadFire_third'],
-    "bodiesFire" : ['spriteMonsterBodyFire_first', 'spriteMonsterBodyFire_second','spriteMonsterBodyFire_third'],
-    "legsFire" : ['spriteMonsterLegsFire_first', 'spriteMonsterLegsFire_second','spriteMonsterLegsFire_third'],
-    "headsHurt" : ['spriteMonsterHeadHurt_first', 'spriteMonsterHeadHurt_second','spriteMonsterHeadHurt_third'],
-    "bodiesHurt" : ['spriteMonsterBodyHurt_first', 'spriteMonsterBodyHurt_second','spriteMonsterBodyHurt_third'],
-    "legsHurt" : ['spriteMonsterLegsHurt_first', 'spriteMonsterLegsHurt_second','spriteMonsterLegsHurt_third'],
-    "headsDie" : ['spriteMonsterHeadDie_first', 'spriteMonsterHeadDie_second','spriteMonsterHeadDie_third'],
-    "bodiesDie" : ['spriteMonsterBodyDie_first', 'spriteMonsterBodyDie_second','spriteMonsterBodyDie_third'],
-    "legsDie" : ['spriteMonsterLegsDie_first', 'spriteMonsterLegsDie_second','spriteMonsterLegsDie_third'],
+  headsIdle: [
+    "spriteMonsterHeadIdle_first",
+    "spriteMonsterHeadIdle_second",
+    "spriteMonsterHeadIdle_third"
+  ],
+  bodiesIdle: [
+    "spriteMonsterBodyIdle_first",
+    "spriteMonsterBodyIdle_second",
+    "spriteMonsterBodyIdle_third"
+  ],
+  legsIdle: [
+    "spriteMonsterLegsIdle_first",
+    "spriteMonsterLegsIdle_second",
+    "spriteMonsterLegsIdle_third"
+  ],
+  headsFire: [
+    "spriteMonsterHeadFire_first",
+    "spriteMonsterHeadFire_second",
+    "spriteMonsterHeadFire_third"
+  ],
+  bodiesFire: [
+    "spriteMonsterBodyFire_first",
+    "spriteMonsterBodyFire_second",
+    "spriteMonsterBodyFire_third"
+  ],
+  legsFire: [
+    "spriteMonsterLegsFire_first",
+    "spriteMonsterLegsFire_second",
+    "spriteMonsterLegsFire_third"
+  ],
+  headsHurt: [
+    "spriteMonsterHeadHurt_first",
+    "spriteMonsterHeadHurt_second",
+    "spriteMonsterHeadHurt_third"
+  ],
+  bodiesHurt: [
+    "spriteMonsterBodyHurt_first",
+    "spriteMonsterBodyHurt_second",
+    "spriteMonsterBodyHurt_third"
+  ],
+  legsHurt: [
+    "spriteMonsterLegsHurt_first",
+    "spriteMonsterLegsHurt_second",
+    "spriteMonsterLegsHurt_third"
+  ],
+  headsDie: [
+    "spriteMonsterHeadDie_first",
+    "spriteMonsterHeadDie_second",
+    "spriteMonsterHeadDie_third"
+  ],
+  bodiesDie: [
+    "spriteMonsterBodyDie_first",
+    "spriteMonsterBodyDie_second",
+    "spriteMonsterBodyDie_third"
+  ],
+  legsDie: [
+    "spriteMonsterLegsDie_first",
+    "spriteMonsterLegsDie_second",
+    "spriteMonsterLegsDie_third"
+  ],
 
-    "firstNames" : ["Kazimir", "Voiclah", "Magamed", "Ludovik", "Genrich", "Sigizmund", "Aslanbek", "Bzdashek"],
-    "secondNames" : ["\"Hriply\"", "\"Shavka\"", "\"Bambula\"", "\"Baklan\"", "\"Brodyaga\"", "\"Vertuhai\""],
-    "thirdNames" : ["Vonuchkin", "Svininsky", "Zhirnidze", "Kakulko", "Soplivkin", "Gryaznulenko", "Potnyakovich"],
+  firstNames: [
+    "Kazimir",
+    "Voiclah",
+    "Magamed",
+    "Ludovik",
+    "Genrich",
+    "Sigizmund",
+    "Aslanbek",
+    "Bzdashek"
+  ],
+  secondNames: [
+    '"Hriply"',
+    '"Shavka"',
+    '"Bambula"',
+    '"Baklan"',
+    '"Brodyaga"',
+    '"Vertuhai"'
+  ],
+  thirdNames: [
+    "Vonuchkin",
+    "Svininsky",
+    "Zhirnidze",
+    "Kakulko",
+    "Soplivkin",
+    "Gryaznulenko",
+    "Potnyakovich"
+  ],
 
-    "backgroundImages" : ['arena1', 'arena2', 'arena3', 'arena4']
+  backgroundImages: ["arena1", "arena2", "arena3", "arena4"]
 };
 
 export const dictTranslateTask = {
-    "dog" : ["собака", "пес", "пёс"],
-    "cat" : ["кот", "кошка", "котэ"],
-    "house" : ["дом"],
-    "spell" : ["заклинание", "чары"],
-    "pig" : ["свинья", "поросенок", "поросёнок", "хрюшка"],
-    "bird" : ["птица"],
-    "towel" : ["полотенце"],
-    "table" : ["стол", "таблица"],
-    "flower" : ["цветок", "цвет"]
+  dog: ["собака", "пес", "пёс"],
+  cat: ["кот", "кошка", "котэ"],
+  house: ["дом"],
+  spell: ["заклинание", "чары"],
+  pig: ["свинья", "поросенок", "поросёнок", "хрюшка"],
+  bird: ["птица"],
+  towel: ["полотенце"],
+  table: ["стол", "таблица"],
+  flower: ["цветок", "цвет"]
 };
 
 export const dictListeningTask = {
-    "audio/listeningTask/elephant.mp3" : "elephant",
-    "audio/listeningTask/forest.mp3" : "forest",
-    "audio/listeningTask/mushroom.mp3" : "mushroom",
-    "audio/listeningTask/ocean.mp3" : "ocean",
-    "audio/listeningTask/rainbow.mp3" : "rainbow",
-    "audio/listeningTask/strawberry.mp3" : "strawberry",
-    "audio/listeningTask/tomato.mp3" : "tomato"
+  "audio/listeningTask/elephant.mp3": "elephant",
+  "audio/listeningTask/forest.mp3": "forest",
+  "audio/listeningTask/mushroom.mp3": "mushroom",
+  "audio/listeningTask/ocean.mp3": "ocean",
+  "audio/listeningTask/rainbow.mp3": "rainbow",
+  "audio/listeningTask/strawberry.mp3": "strawberry",
+  "audio/listeningTask/tomato.mp3": "tomato"
 };
 
 export const dictCapitalsTask = {
-    "img/capitalsTask/spain.jpg" : ["madrid"],
-    "img/capitalsTask/belarus.jpeg" : ["minsk"],
-    "img/capitalsTask/canada.jpg" : ["ottawa", "ottava"],
-    "img/capitalsTask/germany.jpg" : ["berlin"],
-    "img/capitalsTask/greatBritain.jpg" : ["london"],
-    "img/capitalsTask/italy.jpg" : ["rome", "rim"],
-    "img/capitalsTask/lithuania.jpg" : ["vilnius", "vilnus"],
-    "img/capitalsTask/sweden.jpg" : ["stockholm", "stokholm", "stokgolm"],
-    "img/capitalsTask/usa.png" : ["washington"]
+  "img/capitalsTask/spain.jpg": ["madrid"],
+  "img/capitalsTask/belarus.jpeg": ["minsk"],
+  "img/capitalsTask/canada.jpg": ["ottawa", "ottava"],
+  "img/capitalsTask/germany.jpg": ["berlin"],
+  "img/capitalsTask/greatBritain.jpg": ["london"],
+  "img/capitalsTask/italy.jpg": ["rome", "rim"],
+  "img/capitalsTask/lithuania.jpg": ["vilnius", "vilnus"],
+  "img/capitalsTask/sweden.jpg": ["stockholm", "stokholm", "stokgolm"],
+  "img/capitalsTask/usa.png": ["washington"]
 };
 
 export const dictSortTask = {
-    "wolf" : ["wolf", "flow"],
-    "bisycle" : ["bisycle"],
-    "clothes" : ["clothes"],
-    "apple" : ["apple"],
-    "violet" : ["violet"],
-    "coffee" : ["coffee"],
-    "market" : ["market"],
-    "mirror" : ["mirror"],
-    "rabbit" : ["rabbit"]
+  wolf: ["wolf", "flow"],
+  bisycle: ["bisycle"],
+  clothes: ["clothes"],
+  apple: ["apple"],
+  violet: ["violet"],
+  coffee: ["coffee"],
+  market: ["market"],
+  mirror: ["mirror"],
+  rabbit: ["rabbit"]
 };
 
 export const dictRedundantTask = {
-    "img/capitalstask/canada.jpg": ["img/capitalstask/canada.jpg", "img/capitalstask/spain.jpg", "img/capitalstask/italy.jpg"],
-    "img/redudanttask/juventus.png": ["img/redudanttask/arsenal.png", "img/redudanttask/juventus.png", "img/redudanttask/liverpool.png"],
-    "img/redudanttask/elephant.jpg": ["img/redudanttask/bear.jpg", "img/redudanttask/lion.jpg", "img/redudanttask/elephant.jpg"],
-    "img/redudanttask/cucumber.jpg": ["img/redudanttask/cucumber.jpg", "img/redudanttask/oranges.jpg", "img/redudanttask/pears.jpg"],
-    "img/redudanttask/lada.jpg": ["img/redudanttask/mersedes.jpg", "img/redudanttask/ferrari.jpg", "img/redudanttask/lada.jpg"],
-    "img/redudanttask/sinica.jpg": ["img/redudanttask/ostrich.jpg", "img/redudanttask/sinica.jpg", "img/redudanttask/pinguin.jpg"]
-
+  "img/capitalstask/canada.jpg": [
+    "img/capitalstask/canada.jpg",
+    "img/capitalstask/spain.jpg",
+    "img/capitalstask/italy.jpg"
+  ],
+  "img/redudanttask/juventus.png": [
+    "img/redudanttask/arsenal.png",
+    "img/redudanttask/juventus.png",
+    "img/redudanttask/liverpool.png"
+  ],
+  "img/redudanttask/elephant.jpg": [
+    "img/redudanttask/bear.jpg",
+    "img/redudanttask/lion.jpg",
+    "img/redudanttask/elephant.jpg"
+  ],
+  "img/redudanttask/cucumber.jpg": [
+    "img/redudanttask/cucumber.jpg",
+    "img/redudanttask/oranges.jpg",
+    "img/redudanttask/pears.jpg"
+  ],
+  "img/redudanttask/lada.jpg": [
+    "img/redudanttask/mersedes.jpg",
+    "img/redudanttask/ferrari.jpg",
+    "img/redudanttask/lada.jpg"
+  ],
+  "img/redudanttask/sinica.jpg": [
+    "img/redudanttask/ostrich.jpg",
+    "img/redudanttask/sinica.jpg",
+    "img/redudanttask/pinguin.jpg"
+  ]
 };
